8c8a88729d73c7d626cbbf9cfe1438c704f52c7d
Philip Van Every <pvanevery@unm.edu>
2017-11-11 16:12:58 -0500

    Revert "weird stage... rolling back soon"
    This reverts commit abc719d08d2cc2d3563cdad0baab3d4f31fb8a27.

---
+++ system/slkinit.c
@@ -0,0 +1,29 @@
+/* slkinit.c - slckinit	*/
+
+#include <xinu.h>
+
+/*------------------------------------------------------------------------
+ *  slkinit  -  Initialize the Spinlock Table
+ *------------------------------------------------------------------------
+ */
+void	slkinit(void)
+{
+	int32 i;					/* iterator over spinlock table entries */
+	struct	lentry	*slkptr;	/* Ptr to spinlock table entry	*/
+
+	/* Initialize the spinlock table entries */
+
+	for (i = 0; i < NSLK; i++){
+		slkptr = &slktab[i];
+		slkptr->lock = UNLOCKED;
+		slkptr->lstate = SLK_FREE;
+		slkptr->lowner = SLK_NEMO;
+	}
+
+	/* Initialize the table lock */
+
+	slkptr = &slktab[SLK_TABLK];
+	slkptr->lock = UNLOCKED;
+	slkptr->lstate = SLK_USED;
+	slkptr->lowner = SLK_NEMO;
+}
