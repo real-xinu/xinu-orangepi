76eee7e598ef1e64c7dca4e6376b51061583f4bc
Philip Van Every <pvanevery@unm.edu>
2018-01-08 09:57:29 -0500

    multicore getdev()

--- system/getdev.c
+++ system/getdev.c
@@ -14,13 +14,13 @@ did32	getdev(
 	intmask		mask;		/* Saved interrupt mask		*/
 	did32		id;		/* Value to return to caller	*/
 
-	mask = disable();
 	for (id = 0; id < NDEVS; id++) {
+		mask = xsec_beg(devtab[id].dvlock);
 		if (strncmp(devname, devtab[id].dvname, DEVNAMLEN)) {
-			restore(mask);
+			xsec_end(mask, devtab[id].dvlock);
 			return id;
 		}
 	}
-	restore(mask);
+	xsec_end(mask, devtab[id].dvlock);
 	return (did32) SYSERR;
 }
