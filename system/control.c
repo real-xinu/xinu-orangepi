0e1634efe816259649d4db69918f81dcdbe2ff22
Philip Van Every <pvanevery@unm.edu>
2018-01-08 09:59:17 -0500

    multicore control()

--- system/control.c
+++ system/control.c
@@ -17,13 +17,12 @@ syscall	control(
 	struct dentry	*devptr;	/* Entry in device switch table	*/
 	int32		retval;		/* Value to return to caller	*/
 
-	mask = disable();
 	if (isbaddev(descrp)) {
-		restore(mask);
 		return SYSERR;
 	}
 	devptr = (struct dentry *) &devtab[descrp];
+	mask = xsec_beg(devptr->dvlock);
 	retval = (*devptr->dvcntl) (devptr, func, arg1, arg2);
-	restore(mask);
+	xsec_end(mask, devptr->dvlock);
 	return retval;
 }
