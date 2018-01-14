a2202b52b6fe49bb22f7871f5d364179b9419dd9
Philip Van Every <pvanevery@unm.edu>
2018-01-14 12:48:15 -0500

    change lock owner to be a core (cid32) instead of a process (pid32) in lock.h (forgot to do this sooner)

--- include/lock.h
+++ include/lock.h
@@ -9,7 +9,7 @@
  */
 struct lentry {
 	int32	lock;		/* Actual lock word used by hardware */
-	pid32	lowner;		/* id of cpu that currently owns lock */
+	cid32	lowner;		/* id of cpu that currently owns lock */
 	uint32	lcount;		/* count locks by lowner */
 	int32	lpad[13];	/* padding to reach size of ERG */
 };
