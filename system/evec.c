325d494014f561a3b20ef2672a3630c5b7cb1535
Mitchell Augustin <mitchell@mitchellaugustin.com>
2023-10-27 20:10:05 -0400

    Fixed evec

--- system/evec.c
+++ system/evec.c
@@ -61,8 +61,6 @@ void initevec(void){
 	for(i = 8; i < EV_SIZE; i++){
 		exp_vector[i] = (uint32)defexp_handler;
 	}
-
-<<<<<<< HEAD
 	/* overwrite with specific exception handlers as needed */
 	exp_vector[RSTH_IND] = (uint32)rst_except;
 	exp_vector[UDIH_IND] = (uint32)udi_except;
